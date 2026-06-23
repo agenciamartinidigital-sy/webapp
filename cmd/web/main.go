3207c0bd1e6008320b43f0028eb3133f5321a971
agenciamartinidigital <agenciamartinidigital@gmail.com>
2026-06-23 06:10:50 -0300

    att

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,14 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"weapp/pkg/config"
 	"weapp/pkg/handlers"
+	"weapp/pkg/render"
 )
 
 const portNumber = ":8080"
 
 // main is the main function
 func main() {
+
+	var app config.AppConfig
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot crreate template cache")
+	}
+
+	app.TemplateCache = tc
+
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
 
