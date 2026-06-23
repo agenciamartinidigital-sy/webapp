4f62729074dbe14f68ab3d28079e0cfe6ea5f28d
agenciamartinidigital <agenciamartinidigital@gmail.com>
2026-06-23 07:19:48 -0300

    Att

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -2,15 +2,44 @@ package handlers
 
 import (
 	"net/http"
+	"weapp/pkg/config"
+	"weapp/pkg/models"
 	"weapp/pkg/render"
 )
 
+// Repo the repository used by the handlers
+var Repo *Repository
+
+// Repository is the repository type
+type Repository struct {
+	App *config.AppConfig
+}
+
+// NewRepo creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// NewHandlers sets the repositoru fro the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
 // Home is the handler for the home page
-func Home(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "home.page.tmpl")
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
 }
 
 // About is the handler for the about page
-func About(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "about.page.tmpl")
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	// perform some logic
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello, again"
+
+	// send the data to the template
+	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
+		StringMap: stringMap,
+	})
 }
